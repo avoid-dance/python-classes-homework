32948b819a2603d037fafdde6dcf961f817bfe3c
kris <novikova.christy@gmail.com>
2026-06-13 15:49:37 +0300

    Задание-2, атрибуты и взаимодействие классов

--- main.py
+++ main.py
@@ -7,6 +7,16 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lecture(self, lecturer, course, grade):
+        if (isinstance(lecturer, Lecturer)
+                and course in self.courses_in_progress
+                and course in lecturer.courses_attached):
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
 
 class Mentor:
     def __init__(self, name, surname):
@@ -22,4 +32,13 @@ class Lecturer(Mentor):
 
 
 class Reviewer(Mentor):
-    pass
+    def rate_hw(self, student, course, grade):
+        if (isinstance(student, Student)
+                and course in self.courses_attached
+                and course in student.courses_in_progress):
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
